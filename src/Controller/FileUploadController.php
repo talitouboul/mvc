6fb3aae24c08d48095a3ba075e134c3f3d6f3c4e
Tali Touboul <talitouboul@hotmail.fr>
2023-06-29 11:54:45 +0200

    Mise à jour de FileUploadController.php

--- src/Controller/FileUploadController.php
+++ src/Controller/FileUploadController.php
@@ -15,9 +15,25 @@ class FileUploadController extends AbstractController
     {
         $message = null;
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (isset($_FILES['fichier'])) {
-                $fileService = new FileService();
-                $message = $fileService->uploadFile($_FILES['fichier']);
+            if
+            (isset($_FILES['fichier'])) {
+                $file = $_FILES['fichier'];
+
+                // Vérification de la taille maximale du fichier
+                $maxFileSize = 1048576; // 1 Mo
+                if ($file['size'] > $maxFileSize) {
+                    $message = 'Le fichier dépasse la taille maximale autorisée.';
+                } else {
+                    // Vérification de l'extension du fichier
+                    $allowedExtensions = ['jpg', 'jpeg', 'png', 'pdf']; // Extensions autorisées
+                    $fileExtension = pathinfo($file['name'], PATHINFO_EXTENSION);
+                    if (!in_array($fileExtension, $allowedExtensions)) {
+                        $message = 'L\'extension du fichier n\'est pas autorisée.';
+                    } else {
+                        $fileService = new FileService();
+                        $message = $fileService->uploadFile($file);
+                    }
+                }
             } else {
                 $message ='Aucun fichier n\'a été soumis.';
             }
